aefb05601e33794a5141cb5f4c1ab62a237266e3
Da1kon <crackeroff@yandex.ru>
2020-11-02 18:46:12 +0300

    Полностью переработан механизм работы. Также: * Добавил настраиваемую задержку в опросе RAC * Аргументы для infobase info теперь в самой структуре. * Добавил метод ShowSummary - выводит наиболее важнуюю информаци. * Добавил простую проверку файла настроек * Добавил метрику "Блокировка начала сеанса" * Дополнил файл шаблона настроек

--- main.go
+++ main.go
@@ -1,40 +1,139 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"os"
-
-	"encoding/json"
-	"golang.org/x/text/encoding/charmap"
-	"log"
-	"os/exec"
-	"strings"
-
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"golang.org/x/text/encoding/charmap"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
 )
 
 type Server struct {
-	PathToRac string `json:"path_to_rac"`
+	PathToRac    string `json:"path_to_rac"`
 	ExporterPort string `json:"exporter_port"`
-	Cluster []struct {
+	Delay        int    `json:"delay"`
+	Cluster      []struct {
 		Cluster  string `json:"cluster"`
 		Name     string `json:"name"`
 		Infobase []struct {
 			Infobase          string `json:"infobase"`
-			User 		      string `json:"user"`
+			User              string `json:"user"`
 			Name              string `json:"name"`
-			Password string `json:"password"`
+			Password          string `json:"password"`
 			ScheduledJobsDeny string `json:"scheduled-jobs-deny"`
+			SessionsDeny      string `json:"sessions-deny"`
+			InfobaseInfoAgrs  []string
 		} `json:"infobase"`
 	} `json:"cluster"`
 }
 
+var SessionsDeny = prometheus.NewGaugeVec(
+	prometheus.GaugeOpts{
+		Name: "SessionsDeny_status",
+		Help: "Блокировка начала сеансов",
+	},
+	[]string{"ibname"},
+)
+var ScheduledJobsDeny = prometheus.NewGaugeVec(
+	prometheus.GaugeOpts{
+		Name: "ScheduledJobsDeny_status",
+		Help: "Блокировка регламентных",
+	},
+	[]string{"ibname"},
+)
 
+//Список кластеров
+func (srv Server) ClusterList() {
+	ClusterList := []string{"cluster", "list"}
+	out, _ := cmdExec(os.Getenv("rac"), ClusterList...)
+	if err := json.Unmarshal(out, &srv.Cluster); err != nil {
+		log.Println("Ошибка разбора списка кластеров")
+		panic(err)
+	}
+}
+
+//Проверка файла настроек
+func (srv Server) CheckSettings() {
+	for i := range srv.Cluster {
+		for j := range srv.Cluster[i].Infobase {
+			if srv.Cluster[i].Infobase[j].User == "" || srv.Cluster[i].Infobase[j].Name == "" {
+				panic("Незаполнен пользователь или имя информационной базы")
+			}
+		}
+	}
+}
+
+//Список баз в кластере
+func (srv Server) InfobaseSummary() {
+	//log.Println("Получение сведений о списке баз", srv.Cluster[0].Name, srv.Cluster[0].Cluster)
+	out, _ := cmdExec(os.Getenv("rac"), srv.GetArgs("SummaryList")...)
+	if out != nil {
+		if err := json.Unmarshal(out, &srv.Cluster[0].Infobase); err != nil {
+			log.Println("Ошибка разбора списка баз")
+			panic(err)
+		}
+	}
+}
+
+//Подробная информация о конкретной базе
+func (srv Server) InfobaseInfo() {
+	for j := range srv.Cluster[0].Infobase {
+		out, sliceOfMaps0 := cmdExec(os.Getenv("rac"), srv.Cluster[0].Infobase[j].InfobaseInfoAgrs...)
+		out, _ = json.Marshal(sliceOfMaps0[0])
+		if err := json.Unmarshal(out, &srv.Cluster[0].Infobase[j]); err != nil {
+			panic(err)
+		}
+		if srv.Cluster[0].Infobase[j].ScheduledJobsDeny == "on" {
+			ScheduledJobsDeny.With(prometheus.Labels{"ibname": srv.Cluster[0].Infobase[j].Name}).Set(1)
+		} else {
+			ScheduledJobsDeny.With(prometheus.Labels{"ibname": srv.Cluster[0].Infobase[j].Name}).Set(0)
+		}
+		if srv.Cluster[0].Infobase[j].SessionsDeny == "on" {
+			SessionsDeny.With(prometheus.Labels{"ibname": srv.Cluster[0].Infobase[j].Name}).Set(1)
+		} else {
+			SessionsDeny.With(prometheus.Labels{"ibname": srv.Cluster[0].Infobase[j].Name}).Set(0)
+		}
+	}
+}
+
+func (srv Server) ShowSummary() {
+	for j := range srv.Cluster {
+		log.Println("Кластер (uuid):", srv.Cluster[j].Name, srv.Cluster[j].Cluster)
+		for i := range srv.Cluster[j].Infobase {
+			log.Println("\tИмя базы (uuid): ", srv.Cluster[j].Infobase[i].Name, srv.Cluster[j].Infobase[i].Infobase)
+			log.Println("\t\t Блокировка регламентных", srv.Cluster[j].Infobase[i].ScheduledJobsDeny)
+			log.Println("\t\t Блокировка начала сеанса", srv.Cluster[j].Infobase[i].SessionsDeny)
+		}
+	}
+}
+
+//Составление аргументов для методов
+func (srv Server) GetArgs(arg string) []string {
+	switch arg {
+	case "SummaryList":
+		SummaryList := []string{"infobase", "summary", "list", "--cluster=" + srv.Cluster[0].Cluster}
+		return SummaryList
+	case "ClusterList":
+		ClusterList := []string{"cluster", "list"}
+		return ClusterList
+	}
+	return nil
+}
+func (srv Server) GetInfobaseInfoAgrs() {
+	for j := range srv.Cluster[0].Infobase {
+		srv.Cluster[0].Infobase[j].InfobaseInfoAgrs = []string{"infobase", "info", "--cluster=" + srv.Cluster[0].Cluster, "--infobase=" + srv.Cluster[0].Infobase[j].Infobase, "--infobase-user=" + srv.Cluster[0].Infobase[j].User, "--infobase-pwd=" + srv.Cluster[0].Infobase[j].Password}
+	}
+}
 func cmdExec(name string, args ...string) (out []byte, sliceOfMaps []map[string]string) {
+
 	result, err := exec.Command(name, args...).Output()
 	if err != nil {
 		log.Println(err)
@@ -105,90 +204,50 @@ func cmdExec(name string, args ...string) (out []byte, sliceOfMaps []map[string]
 }
 
 func main() {
-
 	log.Println("Инициализация")
-	os.Setenv("rac", "C:/Program Files/1cv8/8.3.16.1063/bin/rac.exe")
 
-	LogFile, err := os.OpenFile("logfile.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0777)
+	jsonFile, err := os.Open("settings.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	log.Println("Successfully Opened settings.json")
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	LogFile, err := os.OpenFile("logfile.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0777)
 	if err != nil {
 		log.Println("Ошибка создания log файла")
 		panic(err)
 	}
 	defer LogFile.Close()
 
-	jsonFile, err := os.Open("settings.json")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("Successfully Opened settings.json")
-	defer jsonFile.Close()
-
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-
 	var srv Server
-
 	if err := json.Unmarshal(byteValue, &srv); err != nil {
-		log.Println("Ошибка настройка")
+		log.Println("Ошибка получения настроек программы")
 		panic(err)
 	}
+	srv.CheckSettings()
 
+	log.Printf("Starting web server at %s\n", srv.ExporterPort)
+	log.SetOutput(LogFile)
 	os.Setenv("rac", srv.PathToRac)
+	prometheus.MustRegister(ScheduledJobsDeny, SessionsDeny)
+
+	srv.ClusterList()
+	srv.InfobaseSummary()
+	srv.GetInfobaseInfoAgrs()
+	srv.InfobaseInfo()
+	srv.ShowSummary()
+
+	fmt.Println("Done")
+
 	var addr = flag.String("listen-address", ":"+srv.ExporterPort, "The address to listen on for HTTP requests.")
-
-	fmt.Println(srv)
-
-	//log.SetOutput(LogFile)
-
-	ScheduledJobsDeny := prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "ScheduledJobsDeny_status",
-			Help: "Блокировка регламентных",
-		},
-		[]string{"ibname"},
-	)
-	prometheus.MustRegister(ScheduledJobsDeny)
-
-	out, _ := cmdExec(os.Getenv("rac"), "cluster", "list")
-	if err := json.Unmarshal(out, &srv.Cluster); err != nil {
-		log.Println("Ошибка разбора списка кластеров")
-		panic(err)
-	}
-
-	out, _ = cmdExec(os.Getenv("rac"), "infobase", "summary", "list", "--cluster="+srv.Cluster[0].Cluster)
-	if out != nil {
-		if err := json.Unmarshal(out, &srv.Cluster[0].Infobase); err != nil {
-			log.Println("Ошибка разбора списка баз")
-			panic(err)
-		}
-	}
-
-	log.Println("Получение сведений о списке баз")
 
 	go func() {
 		for {
-			for j := range srv.Cluster[0].Infobase {
-				if srv.Cluster[0].Infobase[j].User != ""{
-					//log.Println("Получаем данные о ", srv.Cluster[0].Name, "- >", srv.Cluster[0].Infobase[j].Name)
-					out, sliceOfMaps0 := cmdExec(os.Getenv("rac"), "infobase", "info", "--cluster="+srv.Cluster[0].Cluster, "--infobase="+srv.Cluster[0].Infobase[j].Infobase, "--infobase-user="+srv.Cluster[0].Infobase[j].User, "--infobase-pwd="+srv.Cluster[0].Infobase[j].Password)
-					if out != nil {
-						out, _ = json.Marshal(sliceOfMaps0[0])
-						if err := json.Unmarshal(out, &srv.Cluster[0].Infobase[j]); err != nil {
-							panic(err)
-						}
-					} else {
-						continue
-					}
-					if srv.Cluster[0].Infobase[j].ScheduledJobsDeny == "on"{
-						ScheduledJobsDeny.With(prometheus.Labels{"ibname": srv.Cluster[0].Infobase[j].Name}).Set(1)
-					} else {
-						ScheduledJobsDeny.With(prometheus.Labels{"ibname": srv.Cluster[0].Infobase[j].Name}).Set(0)
-					}
-
-				} else {
-					continue
-				}
-
-			}
+			srv.InfobaseInfo()
+			log.Println("sleep")
+			time.Sleep(time.Duration(srv.Delay) * time.Second)
 		}
 	}()
 
